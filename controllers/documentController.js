41b86fbbd48edb319c6a35b377ec9bf8439c328a
chetan gupta <chetangupta123raj@gmail.com>
2023-07-25 20:36:48 +0530

    updated documentController.js

--- controllers/documentController.js
+++ controllers/documentController.js
@@ -28,6 +28,24 @@ const createDocument = async (req, res) => {
     }
 };
 
+const collaborate = async (req, res) => {
+    const username = req.user.username;
+    const documentId = req.params.documentId;
+    try {
+        const document = await Document.findById(documentId);
+        if (!document) {
+            return res.status(404).json({ error: 'Document not found' });
+        }
+        if (document.owner.toString() !== username && !document.collaborators.includes(username)) {
+            return res.status(403).json({ error: 'You are not authorized to collaborate on this document' });
+        }
+        res.status(200).json({ message: `${username} successfully joined the document` });
+    } catch (error) {
+        res.status(500).json({ error: `${username} failed to join on the document` });
+    }
+  }
+
 module.exports = {
     createDocument,
+    collaborate
 };
