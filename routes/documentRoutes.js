d1ec940580f664e2028e5e5f9756ea5d754b8994
chetan gupta <chetangupta123raj@gmail.com>
2023-07-25 20:37:00 +0530

    updated documentRoutes.js

--- routes/documentRoutes.js
+++ routes/documentRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { createDocument } = require('../controllers/documentController');
+const { createDocument, collaborate } = require('../controllers/documentController');
 const { authenticateJwt } = require('../middleware/auth');
 
 const router = express.Router();
 
 router.post('/api/document', authenticateJwt, createDocument);
+router.post('/api/document/collaborate/:documentId', authenticateJwt, collaborate);
 
 module.exports = router;
