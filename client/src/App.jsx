c09fdcf5e37b2e6e12904f94dd98b3b4f0e344df
chetanguptaa <chetangupta123raj@gmail.com>
2023-07-28 19:20:08 +0530

    updated App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,22 @@
 import './App.css';
-import Document from './components/Document';
+// import Document from './components/Document';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { LandingPage } from './components/LandingPage';
+import { NewDoc } from './components/NewDoc';
+
 
 function App() {
   return (
-    <div>
-      <Document />
-    </div>
-  )
+    <>
+      <Router>
+            <Routes>
+                <Route path="/" element={<LandingPage />} />
+                <Route path="/document" element={<NewDoc />} />
+            </Routes>
+      </Router> 
+    </>
+  );
 }
 
 export default App
