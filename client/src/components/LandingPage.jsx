1a5c891f3972b86a08511eea7a1f1aca2a33fc92
chetanguptaa <chetangupta123raj@gmail.com>
2023-07-28 19:20:50 +0530

    added LandingPage.jsx

---
+++ client/src/components/LandingPage.jsx
@@ -0,0 +1,34 @@
+import Card from "react-bootstrap/Card";
+import { Link } from "react-router-dom";
+import './landingPage.css';
+
+export const LandingPage = () => {
+  return (
+    <>
+        <div className="container d-flex align-items-center justify-content-center" style={{ height: "100vh" }}>
+            <div className="row">
+                <div className="col-md-8">
+                    <Link to="/login">
+                        <Card>
+                        <Card.Body>
+                            <Card.Title>Log In</Card.Title>
+                            <Card.Text>Click here to log in</Card.Text>
+                        </Card.Body>
+                        </Card>
+                    </Link>
+                </div>
+                <div className="col-md-8">
+                    <Link to="/signup">
+                        <Card>
+                        <Card.Body>
+                            <Card.Title>Sign Up</Card.Title>
+                            <Card.Text>Click here to sign up</Card.Text>
+                        </Card.Body>
+                        </Card>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    </>
+  );
+};
