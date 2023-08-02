0c6e9d158f6d1224ef13494bf1901a3be77f81a3
chetan gupta <chetangupta123raj@gmail.com>
2023-08-02 14:50:35 +0530

    updated Document.jsx

--- client/src/components/Document.jsx
+++ client/src/components/Document.jsx
@@ -1,5 +1,5 @@
 import Editor from '@monaco-editor/react';
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
 import * as Y from 'yjs';
 import { WebrtcProvider } from 'y-webrtc';
 import { MonacoBinding } from 'y-monaco';
@@ -10,9 +10,27 @@ import axios from "axios";
 
 export const Document = () => {
 
+  const generateShareableLink = () => {
+    if (!title) return ""; // Return an empty link if the title is empty
+    const documentId = generateUniqueIdentifier();
+    return `http://localhost:8000/documents/${documentId}`;
+  };
+
+  // Function to generate a unique identifier (you can use any other method)
+  const generateUniqueIdentifier = () => {
+    return Math.random().toString(36).substring(2, 9);
+  };
+
   const [title, setTitle] = useState("");
+  const [shareableLink, setShareableLink] = useState("");
+
+  useEffect(() => {
+    // Generate the shareable link when the document title changes
+    setShareableLink(generateShareableLink());
+  }, [title]);
 
   const editorRef = useRef(null);
+  const docRef = useRef(null);
 
   // Editor value => A YJS text value ( A Text Value shared by multiple people )
   // One Person deletes text => Deletes from the overall shared text value
@@ -23,7 +41,7 @@ export const Document = () => {
     editorRef.current = editor;
     // Initialize YJS
     const doc = new Y.Doc(); // A Collection of shared object -> Text
-
+    docRef.current = doc;
     // Connect to peers ( or start connection ) with webRTC
     const provider = new WebrtcProvider(title, doc); 
     const type = doc.getText("monaco"); // doc { "monaco" : "What our IDE is showing "}
@@ -33,6 +51,31 @@ export const Document = () => {
     console.log(provider.awareness);
     console.log(binding);
     console.log(monaco);
+  }
+
+  const handleAddDocument = async () => {
+    if (editorRef.current && docRef.current) {
+      const content = editorRef.current.getValue();
+      const yText = docRef.current.getText("monaco");
+      yText.delete(0, yText.length);
+      yText.insert(0, content);
+    }
+    const documentId = generateUniqueIdentifier();
+    const content = editorRef.current.getValue();
+    await axios.post(
+      "http://localhost:8000/api/document",
+      {
+        id: documentId,
+        title: title,
+        content: content,
+      },
+      {
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("token"),
+        },
+      }
+    );
+    alert("Added Document!");
   }
 
   return (
@@ -50,18 +93,13 @@ export const Document = () => {
       <Button
         size={"large"}
         variant="contained"
-        onClick={async () => {
-            await axios.post("http://localhost:8000/api/document", {
-                title: title,
-                // content: content,
-            }, {
-                headers: {
-                    "Authorization": "Bearer " + localStorage.getItem("token")
-                }
-            });
-            alert("Added Document!");
-        }}
+        onClick={handleAddDocument}
       > Add Document </Button>
+      {shareableLink && (
+        <p>
+          Shareable Link: <a href={shareableLink}>{shareableLink}</a>
+        </p>
+      )}
       <Editor
         height="100vh"
         width="100vw"
