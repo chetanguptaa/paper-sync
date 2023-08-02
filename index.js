f536871fa116d50ab68c15bdd7484b6fdc9751ac
chetan gupta <chetangupta123raj@gmail.com>
2023-08-02 16:16:36 +0530

    updated index.js

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const config = require('config');
-const http = require('http');
 const log = require('./utils/logger');
 const { connect } = require('./utils/connect');
 const userRouter = require('./routes/userRoutes');
 const documentRouter = require('./routes/documentRoutes');
-const socketSetup = require('./socket/socket.io.js');
 const cors = require('cors');
 const app = express();
 app.use(cors());
@@ -14,13 +12,9 @@ app.use(express.json());
 app.use(userRouter);
 app.use(documentRouter);
 
-const server = http.createServer(app);
-
-socketSetup(server);
-
 const PORT = config.get("port");
 
-server.listen(PORT, async () => {
+app.listen(PORT, async () => {
   await connect();
   log.info(`app is listening on port ${PORT}`);
 })
